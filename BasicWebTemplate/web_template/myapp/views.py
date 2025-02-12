d6cd75563d6c999edf9494ad6f5caeecbea506c5
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2025-02-12 17:34:09 +0100

    basic populate db

--- BasicWebTemplate/web_template/myapp/views.py
+++ BasicWebTemplate/web_template/myapp/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Item
+from .forms import ItemForm
 
-# Create your views here.
-def myapp(request):
-    return render(request, "myapp.html")
+
+class ItemListView(ListView):
+    model = Item
+    template_name = "item_list.html"
+    context_object_name = "items"
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "item_detail.html"
+
+class ItemCreateView(LoginRequiredMixin, CreateView):
+    model = Item
+    form_class = ItemForm
+    template_name = "item_form.html"
+    success_url = reverse_lazy("item_list")
+
+class ItemUpdateView(LoginRequiredMixin, UpdateView):
+    model = Item
+    form_class = ItemForm
+    template_name = "item_form.html"
+    success_url = reverse_lazy("item_list")
+
+class ItemDeleteView(LoginRequiredMixin, DeleteView):
+    model = Item
+    template_name = "item_confirm_delete.html"
+    success_url = reverse_lazy("item_list")
