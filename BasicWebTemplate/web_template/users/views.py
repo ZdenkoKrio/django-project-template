9d229402d81c811f569e9b2f38cdf00f8731a5da
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2025-02-12 17:13:11 +0100

    basic autentification

--- BasicWebTemplate/web_template/users/views.py
+++ BasicWebTemplate/web_template/users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'profile.html'
